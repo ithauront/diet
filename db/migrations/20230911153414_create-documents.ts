41e679b062ad681699675fb1de51ebb0f140b47e
IURI <123806396+ithauront@users.noreply.github.com>
2023-09-12 16:09:03 +0200

    routes to test and env and env schema

--- db/migrations/20230911153414_create-documents.ts
+++ db/migrations/20230911153414_create-documents.ts
@@ -1,5 +1,16 @@
 import { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('dietLog', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id')
+    table.text('title').notNullable()
+    table.text('description').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.string('isPartOfDiet').checkIn(['yes', 'no']).notNullable()
+  })
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('dietLog')
+}
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const meals = await knex('dietLog').select('*')
+  return meals
+})
+
+app.post('/hello', async () => {
+  const meal = await knex('dietLog').insert({
+    id: crypto.randomUUID(),
+    title: 'refeição de teste',
+    description: 'hamburguer',
+    isPartOfDiet: 'no',
+  })
+  return meal
 })
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('http server running')
