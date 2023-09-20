8d57e6217bb16d3def7bb2e7bc3159583e65eaaf
IURI <123806396+ithauront@users.noreply.github.com>
2023-09-21 00:59:55 +0200

    get single meal test

--- tests/diet.test.ts
+++ tests/diet.test.ts
@@ -2,7 +2,6 @@ import { afterAll, beforeAll, test, describe, beforeEach, expect } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 import { execSync } from 'node:child_process'
-import { get } from 'node:http'
 
 describe('dietRoutes', () => {
   beforeAll(async () => {
@@ -62,7 +61,7 @@ describe('dietRoutes', () => {
     })
   })
   describe('get routes in diet', () => {
-    test('list all the meals', async () => {
+    test.skip('list all the meals', async () => {
       const createUserResponse = await request(app.server).post('/users').send({
         userName: 'test user',
         userEmail: 'tefasxasklngfae@test342.com',
@@ -92,6 +91,42 @@ describe('dietRoutes', () => {
           timeOfMeal: '12:25',
         }),
       ])
+    })
+    test('list a single meal', async () => {
+      const createUserResponse = await request(app.server).post('/users').send({
+        userName: 'test user',
+        userEmail: 'tefasxasklngfae@test342.com',
+        userPassword: 'testasjenshasf', // its important to remember the schemes for validation of email and passoword to avoid problems in the tests
+      })
+      const cookie = createUserResponse.get('Set-Cookie')
+
+      await request(app.server).post('/diet').set('Cookie', cookie).send({
+        title: 'testMeal',
+        description: 'a meal for test',
+        isPartOfDiet: 'yes',
+        dateOfMeal: '12/12/2020',
+        timeOfMeal: '12:25',
+      })
+
+      const getMealResponse = await request(app.server)
+        .get('/diet')
+        .set('Cookie', cookie)
+      const mealId = getMealResponse.body[0].id
+
+      const singleMeal = await request(app.server)
+        .get(`/diet/${mealId}`)
+        .set('Cookie', cookie)
+        .expect(200)
+
+      expect(singleMeal.body).toEqual(
+        expect.objectContaining({
+          title: 'testMeal',
+          description: 'a meal for test',
+          isPartOfDiet: 'yes',
+          dateOfMeal: '12/12/2020',
+          timeOfMeal: '12:25',
+        }),
+      )
     })
   })
 })
