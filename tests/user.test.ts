c57ccc7550884373a369775675b75372ffe0c922
IURI <123806396+ithauront@users.noreply.github.com>
2023-09-20 12:45:38 +0200

    user creation test
    made the first test

---
+++ tests/user.test.ts
@@ -0,0 +1,24 @@
+import { afterAll, beforeAll, test, describe } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('userRoutes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('create new user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        userName: 'test user',
+        userEmail: 'test@test342.com',
+        userPassword: 'testasjenshasf', // its important to remember the schemes for validation of email and passoword to avoid problems in the tests
+      })
+      .expect(201)
+  })
+})
